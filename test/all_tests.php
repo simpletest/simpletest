22f233aa6a11e597367eb3d434cf657fbbf19c96
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2003-05-18 00:48:26 +0000

    Making the top level tests more flexible

--- test/all_tests.php
+++ test/all_tests.php
@@ -4,16 +4,13 @@
     if (!defined("SIMPLE_TEST")) {
         define("SIMPLE_TEST", "../");
     }
+    define("TEST_RUNNING", true);
+    
     require_once(SIMPLE_TEST . 'simple_unit.php');
     require_once(SIMPLE_TEST . 'simple_html_test.php');
     require_once(SIMPLE_TEST . 'simple_mock.php');
-    
-    if (!defined("TEST_RUNNING")) {
-        define("TEST_RUNNING", true);
-        
-        require_once('unit_tests.php');
-        require_once('boundary_tests.php');
-    }
+    require_once('unit_tests.php');
+    require_once('boundary_tests.php');
         
     class AllTests extends GroupTest {
         function AllTests() {
