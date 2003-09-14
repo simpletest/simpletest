5bea941bd7216e2a5fa04248b66a7a77e1eae7fb
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2003-09-14 17:51:32 +0000

    Adding error trapping

--- errors.php
+++ errors.php
@@ -5,26 +5,66 @@
         define('SIMPLE_TEST', './');
     }
     
+    /**
+     *    Singleton error queue used to record trapped
+     *    errors.
+     */
     class SimpleErrorQueue {
         var $_queue;
         
+        /**
+         *    Starts with an empty queue.
+         *    @public
+         */
         function SimpleErrorQueue() {
             $this->clear();
         }
+        
+        /**
+         *    Adds an error to the front of the queue.
+         *    @param $severity        PHP error code.
+         *    @param $message         Text of error.
+         *    @param $filename        File error occoured in.
+         *    @param $line            Line number of error.
+         *    @param $super_globals   Hash of PHP super global arrays.
+         *    @public
+         */
         function add($severity, $message, $filename, $line, $super_globals) {
             array_push(
                     $this->_queue,
                     array($severity, $message, $filename, $line, $super_globals));
         }
+        
+        /**
+         *    Pulls the earliest error from the queue.
+         *    @return     False if none, or a list of error
+         *                information. Elements are: severity
+         *                as the PHP error code, the error message,
+         *                the file with the error, the line number
+         *                and a list of PHP super global arrays.
+         *    @public
+         */
         function extract() {
             if (count($this->_queue)) {
                 return array_shift($this->_queue);
             }
             return false;
         }
+        
+        /**
+         *    Discards the contents of the error queue.
+         *    @public
+         */
         function clear() {
             $this->_queue = array();
         }
+        
+        /**
+         *    Global access to a single error queue.
+         *    @return        Global error queue object.
+         *    @public
+         *    @static
+         */
         function &instance() {
             static $queue = false;
             if (!$queue) {
