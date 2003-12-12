88b27f36d363c7a45bb6d8084cbd3daf5abfbe0a
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2003-12-12 01:10:51 +0000

    Refactoring events in reporter machinery

--- test/visual_test.php
+++ test/visual_test.php
@@ -18,6 +18,7 @@
     require_once(SIMPLE_TEST . 'unit_tester.php');
     require_once(SIMPLE_TEST . 'mock_objects.php');
     require_once(SIMPLE_TEST . 'reporter.php');
+    require_once(SIMPLE_TEST . 'xml.php');
     
     class TestDisplayClass {
         var $_a;
