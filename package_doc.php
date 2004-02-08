c583a652bec25e36e93b24aa880a65ebb74a5d6a
jsweat <jsweat@9ad96551-d73f-0410-9433-afe301b4b3ac>
2004-02-08 19:57:36 +0000

    add package level only file

---
+++ package_doc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Bogus file for package level documentation.
+ *
+ * There is currently a bug in the phpDocumentor version 1.2.3 that if there
+ * is no package level only (i.e. no subpackage specified) data then the 
+ * tutorial branch of the left_nav tree does not appear.
+ *
+ * @todo	Log a bug with the phpDocumentor bug tracker
+ * @author	Jason E. Sweat
+ * @since	2004-02-08
+ * @package	SimpleTest
+ * @version	$Id$
+ */
+
+#?>
