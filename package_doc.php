4ad19a89ce43b8c7f1ea62c937d285df193fd56a
jsweat <jsweat@9ad96551-d73f-0410-9433-afe301b4b3ac>
2004-02-09 05:17:30 +0000

    added GroupTests page

--- package_doc.php
+++ package_doc.php
@@ -5,8 +5,9 @@
  * There is currently a bug in the phpDocumentor version 1.2.3 that if there
  * is no package level only (i.e. no subpackage specified) data then the 
  * tutorial branch of the left_nav tree does not appear.
+ * See this {@link http://sourceforge.net/tracker/index.php?func=detail&aid=893186&group_id=11194&atid=111194 phpdocu bug report}.
  *
- * @todo	Log a bug with the phpDocumentor bug tracker
+ * 
  * @author	Jason E. Sweat
  * @since	2004-02-08
  * @package	SimpleTest
