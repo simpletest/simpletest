801c0a122684505d57f277860802ae839822ce5f
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2004-08-21 14:48:35 +0000

    Working on code generating the docs

---
+++ packages/make_phpdoc_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+Xalan -o ../docs/pkg/QuickStart.pkg ../docs/source/simple_test.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/Overview.pkg ../docs/source/overview.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/UnitTestCase.pkg ../docs/source/unit_test_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/GroupTests.pkg ../docs/source/group_test_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/ServerStubs.pkg ../docs/source/server_stubs_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/MockObjects.pkg ../docs/source/mock_objects_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/PartialMock.pkg ../docs/source/partial_mocks_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/Reporting.pkg ../docs/source/reporter_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/Expectations.pkg ../docs/source/expectation_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/WebTester.pkg ../docs/source/web_tester_documentation.xml phpdoc_docs.xslt
+Xalan -o ../docs/pkg/FormTesting.pkg ../docs/source/form_testing_documentation.xml phpdoc_docs.xslt
