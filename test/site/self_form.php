2f1e610ca6d094109455dda0c3af20f645f02110
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2005-04-29 23:17:47 +0000

    Added web site test scripts

---
+++ test/site/self_form.php
@@ -0,0 +1,19 @@
+<?php
+    if (count($HTTP_GET_VARS) > 0) {
+        $_GET = $HTTP_GET_VARS;
+    }
+?><html>
+    <head><title>Test of form self submission</title></head>
+    <body>
+        <form>
+            <input type="hidden" name="secret" value="Wrong form">
+        </form>
+        <p><?php print $_GET['secret']; ?></p>
+        <p><?php print $_GET['again']; ?></p>
+        <form>
+            <input type="hidden" name="secret" value="Submitted">
+            <input type="submit" name="again">
+        </form>
+        <!-- Bad form closing tag --></form>
+    </body>
+</html>
