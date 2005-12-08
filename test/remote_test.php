4bfe5137ef9bbe00c55e556b1b510190f949d3ac
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2005-12-08 19:35:07 +0000

    Added test running in detached process

--- test/remote_test.php
+++ test/remote_test.php
@@ -2,16 +2,21 @@
     // $Id$
     require_once('../remote.php');
     require_once('../reporter.php');
-    
+
     // The following URL will depend on your own installation.
-    $base_url = 'http://uno/simple/';
-    
+    if (isset($_SERVER['SCRIPT_URI'])) {
+        $base_uri = $_SERVER['SCRIPT_URI'];
+    } elseif (isset($_SERVER['HTTP_HOST']) && isset($_SERVER['PHP_SELF'])) {
+        $base_uri = 'http://'. $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+    };
+    $test_url = str_replace('remote_test.php', 'visual_test.php', $base_uri);
+
     $test = &new GroupTest('Remote tests');
     $test->addTestCase(new RemoteTestCase(
-            $base_url . 'test/visual_test.php?xml=yes',
-            $base_url . 'test/visual_test.php?xml=yes&dry=yes'));
+            $test_url . '?xml=yes',
+            $test_url . '?xml=yes&dry=yes'));
     if (SimpleReporter::inCli()) {
-        exit ($test->run(new XmlReporter()) ? 0 : 1);
+        exit ($test->run(new TextReporter()) ? 0 : 1);
     }
     $test->run(new HtmlReporter());
 ?>
