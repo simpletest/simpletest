cc647b083f3808611c80688bb3062f0aff1d3d33
pp11 <pp11@9ad96551-d73f-0410-9433-afe301b4b3ac>
2007-05-09 11:15:52 +0000

    New DomTestCase with CSS selectors

---
+++ extensions/dom_tester/dom_tester_test.php
@@ -0,0 +1,134 @@
+<?php
+
+// $Id$
+
+require_once(dirname(__FILE__) . '/../../autorun.php');
+require_once(dirname(__FILE__) . '/../dom_tester.php');
+
+class TestOfLiveCssSelectors extends DomTestCase {
+    function setUp() {
+        $this->addHeader('User-Agent: SimpleTest ' . SimpleTest::getVersion());
+    }
+    
+    function testGet() {
+        $url = 'http://simpletest.org/live/extensions/dom_tester/dom_tester.html';
+        $this->assertTrue($this->get($url));
+        $this->assertEqual($this->getUrl(), $url);
+        $this->assertElementsBySelector('h1', array('Test page'));
+        $this->assertElementsBySelector('ul#list li a[href]', array('link'));
+        $this->assertElementsBySelector('body  h1', array('Test page'));
+        $this->assertElementsBySelector('#mybar', array('myfoo bis'));
+    }
+}
+
+class TestOfCssSelectors extends UnitTestCase {
+
+	function TestOfCssSelectors() {
+		$html = file_get_contents(dirname(__FILE__) . '/dom_tester.html');
+		$this->dom = new DomDocument('1.0', 'utf-8');
+		$this->dom->validateOnParse = true;
+		$this->dom->loadHTML($html);
+	}
+
+    function testBasicSelector() {
+        $expectation = new CssSelectorExpectation($this->dom, 'h1');
+        $this->assertTrue($expectation->test(array('Test page')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'h2');
+        $this->assertTrue($expectation->test(array('Title 1', 'Title 2')));
+
+		$expectation = new CssSelectorExpectation($this->dom, '#footer');
+        $this->assertTrue($expectation->test(array('footer')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'div#footer');
+        $this->assertTrue($expectation->test(array('footer')));
+
+		$expectation = new CssSelectorExpectation($this->dom, '.header');
+        $this->assertTrue($expectation->test(array('header')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'p.header');
+        $this->assertTrue($expectation->test(array('header')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'div.header');
+        $this->assertTrue($expectation->test(array()));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#mylist ul li');
+        $this->assertTrue($expectation->test(array('element 3', 'element 4')));
+
+		$expectation = new CssSelectorExpectation($this->dom, '#nonexistant');
+        $this->assertTrue($expectation->test(array()));
+    }
+    
+    function testAttributeSelectors() {
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[href]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[class~="foo1"]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[class~="bar1"]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[class~="foobar1"]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[class^="foo1"]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[class$="foobar1"]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[class*="oba"]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[href="http://www.google.com/"]');
+        $this->assertTrue($expectation->test(array('link')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#anotherlist li a[class|="bar1"]');
+        $this->assertTrue($expectation->test(array('another link')));  	
+
+		$expectation = new CssSelectorExpectation($this->dom, 'ul#list li a[class*="oba"][class*="ba"]');
+        $this->assertTrue($expectation->test(array('link')));  	
+
+		$expectation = new CssSelectorExpectation($this->dom, 'p[class="myfoo"][id="mybar"]');
+        $this->assertTrue($expectation->test(array('myfoo bis')));  	
+
+		$expectation = new CssSelectorExpectation($this->dom, 'p[onclick*="a . and a #"]');
+        $this->assertTrue($expectation->test(array('works great')));  	
+    }
+    
+    function testCombinators() {
+		$expectation = new CssSelectorExpectation($this->dom, 'body  h1');
+        $this->assertTrue($expectation->test(array('Test page')));  	
+
+		$expectation = new CssSelectorExpectation($this->dom, 'div#combinators > ul  >   li');
+        $this->assertTrue($expectation->test(array('test 1', 'test 2')));  	
+
+		$expectation = new CssSelectorExpectation($this->dom, 'div#combinators>ul>li');
+        $this->assertTrue($expectation->test(array('test 1', 'test 2')));
+        
+		$expectation = new CssSelectorExpectation($this->dom, 'div#combinators ul  +   li');
+        $this->assertTrue($expectation->test(array('test 1', 'test 3')));
+        
+		$expectation = new CssSelectorExpectation($this->dom, 'div#combinators ul+li');
+        $this->assertTrue($expectation->test(array('test 1', 'test 3')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'h1, h2');
+        $this->assertTrue($expectation->test(array('Test page', 'Title 1', 'Title 2')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'h1,h2');
+        $this->assertTrue($expectation->test(array('Test page', 'Title 1', 'Title 2')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'h1  ,   h2');
+        $this->assertTrue($expectation->test(array('Test page', 'Title 1', 'Title 2')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'h1, h1,h1');
+        $this->assertTrue($expectation->test(array('Test page')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'h1,h2,h1');
+        $this->assertTrue($expectation->test(array('Test page', 'Title 1', 'Title 2')));
+
+		$expectation = new CssSelectorExpectation($this->dom, 'p[onclick*="a . and a #"], div#combinators > ul + li');
+        $this->assertTrue($expectation->test(array('works great', 'test 1')));
+    }
+}
