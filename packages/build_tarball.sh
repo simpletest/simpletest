245e6559699ec6ae0235996c555cc9ba64fc090c
lastcraft <lastcraft@9ad96551-d73f-0410-9433-afe301b4b3ac>
2007-07-17 02:46:14 +0000

    Building tarball

--- packages/build_tarball.sh
+++ packages/build_tarball.sh
@@ -5,92 +5,98 @@
 cd ../..
 
 NAME=simpletest_`cat simpletest/VERSION`.tar.gz
-FILES=(simpletest/compatibility.php \
-          simpletest/collector.php \
-          simpletest/dumper.php \
-          simpletest/errors.php \
-          simpletest/exceptions.php \
-          simpletest/expectation.php \
-          simpletest/socket.php \
-          simpletest/encoding.php \
-          simpletest/url.php \
-          simpletest/cookies.php \
-          simpletest/http.php \
-          simpletest/authentication.php \
-          simpletest/user_agent.php \
-          simpletest/browser.php \
-          simpletest/parser.php \
-          simpletest/tag.php \
-          simpletest/selector.php \
-          simpletest/form.php \
-          simpletest/frames.php \
-          simpletest/page.php \
-          simpletest/reflection_php4.php \
-          simpletest/reflection_php5.php \
-          simpletest/invoker.php \
-          simpletest/scorer.php \
-          simpletest/reporter.php \
-          simpletest/mock_objects.php \
-          simpletest/simpletest.php \
-          simpletest/test_case.php \
-          simpletest/unit_tester.php \
-          simpletest/web_tester.php \
-          simpletest/shell_tester.php \
-          simpletest/xml.php \
-          simpletest/detached.php \
-          simpletest/remote.php \
-          simpletest/eclipse.php \
-          simpletest/extensions/pear_test_case.php \
-          simpletest/extensions/phpunit_test_case.php \
-          simpletest/README \
+FILES=(simpletest/README \
           simpletest/VERSION \
           simpletest/LICENSE \
           simpletest/HELP_MY_TESTS_DONT_WORK_ANYMORE \
-          simpletest/test/all_tests.php \
-          simpletest/test/unit_tests.php \
+          simpletest/authentication.php \
+          simpletest/autorun.php \
+          simpletest/browser.php \
+          simpletest/collector.php \
+          simpletest/compatibility.php \
+          simpletest/cookies.php \
+          simpletest/default_reporter.php \
+          simpletest/detached.php \
+          simpletest/dumper.php \
+          simpletest/eclipse.php \
+          simpletest/encoding.php \
+          simpletest/errors.php \
+          simpletest/exceptions.php \
+          simpletest/expectation.php \
+          simpletest/form.php \
+          simpletest/frames.php \
+          simpletest/http.php \
+          simpletest/invoker.php \
+          simpletest/mock_objects.php \
+          simpletest/page.php \
+          simpletest/parser.php \
+          simpletest/reflection_php4.php \
+          simpletest/reflection_php5.php \
+          simpletest/remote.php \
+          simpletest/reporter.php \
+          simpletest/scorer.php \
+          simpletest/selector.php \
+          simpletest/shell_tester.php \
+          simpletest/simpletest.php \
+          simpletest/socket.php \
+          simpletest/tag.php \
+          simpletest/test_case.php \
+          simpletest/unit_tester.php \
+          simpletest/url.php \
+          simpletest/user_agent.php \
+          simpletest/web_tester.php \
+          simpletest/xml.php \
+          simpletest/extensions/pear_test_case.php \
+          simpletest/extensions/phpunit_test_case.php \
           simpletest/test/acceptance_test.php \
+          simpletest/test/adapter_test.php \
+          simpletest/test/all_tests.php \
+          simpletest/test/authentication_test.php \
+          simpletest/test/bad_test_suite.php \
+          simpletest/test/browser_test.php \
           simpletest/test/collector_test.php \
-          simpletest/test/simpletest_test.php \
+          simpletest/test/command_line_test.php \
+          simpletest/test/compatibility_test.php \
+          simpletest/test/cookies_test.php \
+          simpletest/test/detached_test.php \
+          simpletest/test/dumper_test.php \
+          simpletest/test/eclipse_test.php \
+          simpletest/test/encoding_test.php \
           simpletest/test/errors_test.php \
           simpletest/test/exceptions_test.php \
-          simpletest/test/compatibility_test.php \
-          simpletest/test/dumper_test.php \
           simpletest/test/expectation_test.php \
-          simpletest/test/adapter_test.php \
-          simpletest/test/socket_test.php \
-          simpletest/test/url_test.php \
-          simpletest/test/cookies_test.php \
-          simpletest/test/encoding_test.php \
-          simpletest/test/http_test.php \
-          simpletest/test/authentication_test.php \
-          simpletest/test/user_agent_test.php \
-          simpletest/test/browser_test.php \
-          simpletest/test/parser_test.php \
-          simpletest/test/tag_test.php \
           simpletest/test/form_test.php \
           simpletest/test/frames_test.php \
+          simpletest/test/http_test.php \
+          simpletest/test/interfaces_test.php \
+          simpletest/test/live_test.php \
+          simpletest/test/mock_objects_test.php \
           simpletest/test/page_test.php \
+          simpletest/test/parse_error_test.php \
+          simpletest/test/parser_test.php \
           simpletest/test/reflection_php4_test.php \
           simpletest/test/reflection_php5_test.php \
-          simpletest/test/mock_objects_test.php \
-          simpletest/test/interfaces_test.php \
-          simpletest/test/visual_test.php \
-          simpletest/test/shell_test.php \
-          simpletest/test/web_tester_test.php \
-          simpletest/test/unit_tester_test.php \
-          simpletest/test/shell_tester_test.php \
-          simpletest/test/xml_test.php \
-          simpletest/test/live_test.php \
-          simpletest/test/parse_error_test.php \
-          simpletest/test/detached_test.php \
           simpletest/test/remote_test.php \
+          simpletest/test/shell_test.php \
+          simpletest/test/shell_tester_test.php \
+          simpletest/test/simpletest_test.php \
+          simpletest/test/socket_test.php \
+          simpletest/test/tag_test.php \
           simpletest/test/test_with_parse_error.php \
+          simpletest/test/unit_tests.php \
+          simpletest/test/unit_tester_test.php \
+          simpletest/test/url_test.php \
+          simpletest/test/user_agent_test.php \
+          simpletest/test/visual_test.php \
+          simpletest/test/web_tester_test.php \
+          simpletest/test/xml_test.php \
           simpletest/test/support/collector/collectable.1 \
           simpletest/test/support/collector/collectable.2 \
           simpletest/test/support/upload_sample.txt \
           simpletest/test/support/supplementary_upload_sample.txt \
           simpletest/test/support/latin1_sample \
           simpletest/test/support/spl_examples.php \
+          simpletest/test/support/empty_test_file.php \
           simpletest/docs/en/docs.css \
           simpletest/docs/en/index.html \
           simpletest/docs/en/overview.html \
