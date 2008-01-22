f12d9df931793218064517267dd36f0df25c00f0
pp11 <pp11@9ad96551-d73f-0410-9433-afe301b4b3ac>
2008-01-22 20:50:23 +0000

    New recursive iterator for scanning all tests in the extensions directory

--- test/extensions_tests.php
+++ test/extensions_tests.php
@@ -1,14 +1,23 @@
 <?php
+// $Id$
 require_once(dirname(__FILE__) . '/../autorun.php');
 require_once(dirname(__FILE__) . '/../collector.php');
 
 class ExtensionsTests extends TestSuite {
+	function skip() {
+		$this->skipIf(version_compare(phpversion(), '5', '<'),
+                      'Many extensions only work with PHP5 and above');
+	}
+
     function ExtensionsTests() {
         $this->TestSuite('Extension tests for SimpleTest ' . SimpleTest::getVersion());
-        $collector = new SimplePatternCollector('/test\.php$/');
-		$this->collect(dirname(__FILE__).'/extensions/testdox', $collector);
-		$this->collect(dirname(__FILE__).'/extensions/treemap_reporter', $collector);
-		$this->collect(dirname(__FILE__).'/extensions/dom_tester', $collector);
+
+		$nodes = new RecursiveDirectoryIterator(dirname(__FILE__).'/../extensions/');
+		foreach(new RecursiveIteratorIterator($nodes) as $node) {
+			if (preg_match('/test\.php$/', $node->getFilename())) {
+		        $this->addFile($node->getPathname());
+			}        	
+        }
     }
 }
 ?>
