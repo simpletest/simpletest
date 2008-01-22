53b4ce0ec9b05021cb3e623ffd12dc32e06a6a85
pp11 <pp11@9ad96551-d73f-0410-9433-afe301b4b3ac>
2008-01-22 20:07:39 +0000

    Moving the "recorder" tests to the /extensions directory

--- extensions/recorder/test/test.php
+++ extensions/recorder/test/test.php
@@ -1,13 +1,13 @@
 <?php
 // $Id: test.php 1500 2007-04-29 14:33:31Z pp11 $
 require_once dirname(__FILE__) . '/../../../autorun.php';
-require_once(dirname(__FILE__) . '/../../../extensions/recorder.php');
+require_once(dirname(__FILE__) . '/../../recorder.php');
 
 class TestOfRecorder extends UnitTestCase {
     
     function testContentOfRecorderWithOnePassAndOneFailure() {
         $test = &new TestSuite();
-        $test->addTestFile(dirname(__FILE__) . '/sample_test.php');
+        $test->addTestFile(dirname(__FILE__) . '/sample.php');
         $recorder = new Recorder();
         $test->run($recorder);
         $this->assertEqual(count($recorder->results), 2);
@@ -15,14 +15,14 @@ class TestOfRecorder extends UnitTestCase {
         $this->assertEqual(count($recorder->results[0]), 4);
         $this->assertPattern("/".substr(time(), 9)."/", $recorder->results[0]['time']);
         $this->assertEqual($recorder->results[0]['status'], "Passed");
-        $this->assertPattern("/test\.php->SampleTestForRecorder->testTrueIsTrue/i", $recorder->results[0]['test']);
-        $this->assertPattern("/ at \[.*recorder\/sample_test\.php line 8\]/", $recorder->results[0]['message']);
+        $this->assertPattern("/sample\.php->SampleTestForRecorder->testTrueIsTrue/i", $recorder->results[0]['test']);
+        $this->assertPattern("/ at \[.*recorder\/test\/sample\.php line 7\]/", $recorder->results[0]['message']);
 
         $this->assertEqual(count($recorder->results[1]), 4);
         $this->assertPattern("/".substr(time(), 9)."/", $recorder->results[1]['time']);
         $this->assertEqual($recorder->results[1]['status'], "Failed");
-        $this->assertPattern("/test\.php->SampleTestForRecorder->testFalseIsTrue/i", $recorder->results[1]['test']);
-        $this->assertPattern("/Expected false, got \[Boolean: true\] at \[.*recorder\/sample_test\.php line 12\]/", $recorder->results[1]['message']);
+        $this->assertPattern("/sample\.php->SampleTestForRecorder->testFalseIsTrue/i", $recorder->results[1]['test']);
+        $this->assertPattern("/Expected false, got \[Boolean: true\] at \[.*recorder\/test\/sample\.php line 11\]/", $recorder->results[1]['message']);
     }
 }
 ?>
