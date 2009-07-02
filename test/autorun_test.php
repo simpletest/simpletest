ba27532bbb25468f6d58b4d17680330b914eae82
dgheath <dgheath@9ad96551-d73f-0410-9433-afe301b4b3ac>
2009-07-02 13:09:03 +0000

    1. change method names to camelCase 2. correct test method name

--- test/autorun_test.php
+++ test/autorun_test.php
@@ -3,18 +3,18 @@ require_once(dirname(__FILE__) . '/../autorun.php');
 require_once(dirname(__FILE__) . '/support/test1.php');
 
 class TestOfAutorun extends UnitTestCase {
-    function test_load_if_included() {
+    function testLoadIfIncluded() {
         $tests = new TestSuite();
         $tests->addFile(dirname(__FILE__) . '/support/test1.php');
         $this->assertEqual($tests->getSize(), 1);
     }
 
-    function test_exit_status_one_if_tests_fail() {
+    function testExitStatusOneIfTestsFail() {
         exec('php ' . dirname(__FILE__) . '/support/failing_test.php', $output, $exit_status);
         $this->assertEqual($exit_status, 1);
     }
 
-    function test_exit_status_zero_if_tests_fail() {
+    function testExitStatusZeroIfTestsPass() {
         exec('php ' . dirname(__FILE__) . '/support/passing_test.php', $output, $exit_status);
         $this->assertEqual($exit_status, 0);
     }
