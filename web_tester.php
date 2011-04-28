66a859f74c7eb6f0d5f3265db30a310d97c460e1
pp11 <pp11@9ad96551-d73f-0410-9433-afe301b4b3ac>
2011-04-28 15:56:11 +0000

    Fix for bug n° : 2896575 - WebTestCase::assertTrue: empty $message - patch submitted by daniel hahler (blueyed)

--- web_tester.php
+++ web_tester.php
@@ -1466,7 +1466,7 @@ class WebTestCase extends SimpleTestCase {
      *    @return boolean           True on pass
      *    @access public
      */
-    function assertTrue($result, $message = false) {
+    function assertTrue($result, $message = '%s') {
         return $this->assert(new TrueExpectation(), $result, $message);
     }
 
