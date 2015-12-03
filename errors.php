450484ac28e98c0d0bd3f500ac4d5129809177fc
jakoch <jakoch@web.de>
2015-12-03 12:52:09 +0100

    temporarily unhide SimpleTest errors for easier debugging

--- errors.php
+++ errors.php
@@ -241,7 +241,8 @@ function SimpleTestErrorHandler($severity, $message, $filename = null, $line = n
     $severity = $severity & error_reporting();
     if ($severity) {
         restore_error_handler();
-        if (IsNotCausedBySimpleTest($message) && IsNotTimeZoneNag($message)) {
+        // disabled filtering of errors caused by SimpleTest itself. too many things hidden for debugging. -- jakoch
+        if (/*IsNotCausedBySimpleTest($message) &&*/ IsNotTimeZoneNag($message)) {
             if (ini_get('log_errors')) {
                 $label = SimpleErrorQueue::getSeverityAsString($severity);
                 error_log("$label: $message in $filename on line $line");
