9ce34cb4468c5a23283c1accc9423333dde380c1
jakoch <jakoch@web.de>
2019-09-18 01:02:55 +0200

    create "build/" dir, move packages/coverage/generate_coverage.sh there [skip ci]

--- build/generate_coverage_report.sh
+++ build/generate_coverage_report.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+
 cd ../..
-php ./extensions/coverage/bin/php-coverage-open.php \
-	--exclude='.*/test/.*' \
-    --exclude='.*DB.common.php$' \
-    --exclude='.*sqlite.php$' \
-    --exclude='.*unit_tests.php$'\
-    --exclude='.*PEAR.php$'
+
+php ./extensions/coverage/bin/php-coverage-open.php --exclude='.*/test/.*' --exclude='.*sqlite.php$' --exclude='.*unit_tests.php$'
 
 # run all tests
 php -d auto_prepend_file=./extensions/coverage/autocoverage.php -f test/unit_tests.php    
