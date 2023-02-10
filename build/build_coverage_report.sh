0d05840d02ad1a02ec525f851279c94e9b778688
Jens A. Koch <jakoch@web.de>
2023-02-10 23:38:21 +0100

    update to new folder structure

--- build/build_coverage_report.sh
+++ build/build_coverage_report.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd ../..
+cd ..
 
-php ./extensions/coverage/bin/php-coverage-open.php --exclude='.*/test/.*' --exclude='.*sqlite.php$' --exclude='.*unit_tests.php$'
+php ./extensions/coverage/bin/php-coverage-open.php --exclude='.*/tests/.*' --exclude='.*sqlite.php$' --exclude='.*unit_tests.php$'
 
 # run all tests
-php -d auto_prepend_file=./extensions/coverage/autocoverage.php -f test/unit_tests.php    
-php -d auto_prepend_file=./extensions/coverage/autocoverage.php -f extensions/coverage/test/test.php    
+php -d auto_prepend_file=./extensions/coverage/autocoverage.php -f tests/unit_tests.php
+php -d auto_prepend_file=./extensions/coverage/autocoverage.php -f extensions/coverage/tests/test.php
 
 php ./extensions/coverage/bin/php-coverage-close.php
 php ./extensions/coverage/bin/php-coverage-report.php
